d4df028589b89195982b7e7580dfa95e4544dc47
DuyPhuc <duyphuc070104@gmail.com>
2024-06-24 21:55:54 +0700

    Update for Fragment Communication

--- app/src/main/java/com/example/exercise03/MainActivity.java
+++ app/src/main/java/com/example/exercise03/MainActivity.java
@@ -1,25 +1,42 @@
 package com.example.exercise03;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.exercise03.content.MovieUtils;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+                    implements RatingFragment.OnFragmentInteractionListener{
     private boolean mTwoPane = false;
+    private Button mButton;
+    private boolean isFragmentDisplayed = false;
+
+    // Saved instance state keys.
+    static final String STATE_FRAGMENT = "state_of_fragment";
+    static final String STATE_CHOICE = "user_choice";
+
+    // The radio button choice default is 2 = (no choice).
+    // Initialize the radio button choice to the default.
+    private int mRadioButtonChoice = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +47,32 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
 
+        // Get the button for opening and closing the fragment.
+        mButton = findViewById(R.id.open_button);
+
+        // If returning from a configuration change, get the
+        // fragment state and set the button text.
+        if (savedInstanceState != null) {
+            isFragmentDisplayed = savedInstanceState.getBoolean(STATE_FRAGMENT);
+            mRadioButtonChoice = savedInstanceState.getInt(STATE_CHOICE);
+            if (isFragmentDisplayed) {
+                // If the fragment is displayed, change button to "close".
+                mButton.setText(R.string.close);
+            }
+        }
+
+        // Set the click listener for the button.
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isFragmentDisplayed) {
+                    displayRatingFragment();
+                } else {
+                    closeRatingFragment();
+                }
+            }
+        });
+
         // Get the movie list as a RecyclerView.
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.movie_list);
         recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(MovieUtils.MOVIE_ITEMS));
@@ -37,6 +80,60 @@ public class MainActivity extends AppCompatActivity {
             mTwoPane = true;
         }
     }
+
+    public void displayRatingFragment() {
+        // Instantiate the fragment.
+        RatingFragment simpleFragment =
+                RatingFragment.newInstance(mRadioButtonChoice);
+        // Get the FragmentManager and start a transaction.
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager
+                .beginTransaction();
+
+        // Add the SimpleFragment.
+        fragmentTransaction.add(R.id.fragment_container,
+                simpleFragment).addToBackStack(null).commit();
+
+        // Update the Button text.
+        mButton.setText(R.string.close);
+        // Set boolean flag to indicate fragment is open.
+        isFragmentDisplayed = true;
+    }
+
+    public void closeRatingFragment() {
+        // Get the FragmentManager.
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        // Check to see if the fragment is already showing.
+        RatingFragment simpleFragment = (RatingFragment) fragmentManager
+                .findFragmentById(R.id.fragment_container);
+        if (simpleFragment != null) {
+            // Create and commit the transaction to remove the fragment.
+            FragmentTransaction fragmentTransaction =
+                    fragmentManager.beginTransaction();
+            fragmentTransaction.remove(simpleFragment).commit();
+        }
+        // Update the Button text.
+        mButton.setText(R.string.open);
+        // Set boolean flag to indicate fragment is closed.
+        isFragmentDisplayed = false;
+    }
+
+    @Override
+    public void onRadioButtonChoice(int choice) {
+        // Keep the radio button choice to pass it back to the fragment.
+        mRadioButtonChoice = choice;
+        // Show a Toast with the radio button choice.
+        Toast.makeText(this, "Choice is " + Integer.toString(choice),
+                LENGTH_SHORT).show();
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save the state of the fragment (true=open, false=closed).
+        savedInstanceState.putBoolean(STATE_FRAGMENT, isFragmentDisplayed);
+        savedInstanceState.putInt(STATE_CHOICE, mRadioButtonChoice);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
     class SimpleItemRecyclerViewAdapter
             extends RecyclerView.Adapter
             <SimpleItemRecyclerViewAdapter.ViewHolder> {
---
+++ app/src/main/java/com/example/exercise03/RatingFragment.java
@@ -0,0 +1,120 @@
+package com.example.exercise03;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class RatingFragment extends Fragment {
+
+    // The radio button choice has 3 states: 0 = yes, 1 = no,
+    // 2 = default (no choice). Using all 3 states.
+    private static final int YES = 0;
+    private static final int NO = 1;
+    private static final int NONE = 2;
+
+    // Initialize the choice to the default (no choice)
+    private int mRadioButtonChoice = NONE;
+
+    // The "choice" key for the bundle.
+    private static final String CHOICE = "choice";
+
+    // The listener interface.
+    OnFragmentInteractionListener mListener;
+
+    public RatingFragment() {
+        // Required empty public constructor
+    }
+
+    interface OnFragmentInteractionListener {
+        void onRadioButtonChoice(int choice);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new ClassCastException(context.toString()
+                    + getResources().getString(R.string.exception_message));
+        }
+    }
+
+    public static RatingFragment newInstance(String param1, String param2) {
+        RatingFragment fragment = new RatingFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View rootView = inflater.inflate(R.layout.fragment_rating,
+                container, false);
+        final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
+        // If user reopens the fragment after making a choice, the bundle
+        // contains "choice".
+        if (getArguments().containsKey(CHOICE)) {
+            // A choice was made, so get the choice.
+            mRadioButtonChoice = getArguments().getInt(CHOICE);
+            // Check the radio button choice.
+            if (mRadioButtonChoice != NONE) {
+                radioGroup.check
+                        (radioGroup.getChildAt(mRadioButtonChoice).getId());
+            }
+        }
+        // Set the radioGroup onCheckedChanged listener.
+        radioGroup.setOnCheckedChangeListener(
+                new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup group, int checkedId) {
+                        View radioButton = radioGroup.findViewById(checkedId);
+                        int index = radioGroup.indexOfChild(radioButton);
+                        TextView textView =
+                                rootView.findViewById(R.id.fragment_header);
+                        switch (index) {
+                            case YES: // User chose "Yes".
+                                textView.setText(R.string.yes_message);
+                                mRadioButtonChoice = YES;
+                                mListener.onRadioButtonChoice(YES);
+                                break;
+                            case NO: // User chose "No".
+                                textView.setText(R.string.no_message);
+                                mRadioButtonChoice = NO;
+                                mListener.onRadioButtonChoice(NO);
+                                break;
+                            default: // No choice made.
+                                mRadioButtonChoice = NONE;
+                                mListener.onRadioButtonChoice(NONE);
+                                break;
+                        }
+                    }
+                });
+
+        // Return the View for the fragment's UI.
+        return rootView;
+    }
+
+    public static RatingFragment newInstance(int choice) {
+        RatingFragment fragment = new RatingFragment();
+        Bundle arguments = new Bundle();
+        arguments.putInt(CHOICE, choice);
+        fragment.setArguments(arguments);
+        return fragment;
+    }
+}
